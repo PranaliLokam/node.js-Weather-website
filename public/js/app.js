e365d16f796508d896e50a4b21216acda57a1350
PranaliLokam <pranalilokam2000@gmail.com>
2023-01-25 01:10:54 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -20,7 +20,7 @@ weatherform.addEventListener('submit', (e) => {            //functions which run
     msgOne.textContent = 'Loading...'
     msgTwo.document = ''
 
-    fetch('http://localhost:3000/weather?address=' + location ).then((response) => {
+    fetch('/weather?address=' + location ).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 msgOne.textContent = data.error;
